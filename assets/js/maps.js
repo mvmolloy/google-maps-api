67358132782d9983ad81c90cd7380bcef1853bf2
mvmolloy <mvamolloy@gmail.com>
2020-04-16 10:32:27 +0000

    moving maps js into js file

---
+++ assets/js/maps.js
@@ -0,0 +1,37 @@
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 3,
+                center: { //set map to start on europe
+                    lat: 50.619261,
+                    lng: 3.134766
+                }
+            });
+
+            var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+
+            var locations = [{ // vu amsterdam 
+                lat: 52.335278,
+                lng: 4.865291
+            },
+            { // ku leuven 
+                lat: 50.878110,
+                lng: 4.700338
+            },
+                { //university of nottingham
+                lat: 52.56296,
+                lng: 1.11453
+            }, { //university of kent
+                lat: 51.298601,
+                lng: 1.063239
+            }];
+
+            var markers = locations.map(function(location, i) {
+                return new google.maps.Marker({
+                    position: location,
+                    label: labels[i % labels.length]
+                });
+            });
+
+            var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+        }
